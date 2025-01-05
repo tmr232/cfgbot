ad05b55be09be90b40d7d257cabea8968fe9eeb2
Tamir Bahar <tamir.bahar@gmail.com>
2025-01-05 11:52:28 +0200

    And now?

--- src/cfgbot/cfgbot.py
+++ src/cfgbot/cfgbot.py
@@ -357,7 +357,7 @@ def find_github_indices() -> list[Path | IndexLocator]:
                 raw_url_base=GHIDRA_RAW_URL_BASE,
             )
         )
-    github_code_indices = list(Path(__file__, "..", "indices").glob("*.json"))
+    github_code_indices = list((Path(__file__).parent / "indices").glob("*.json"))
     log.info("looking for GH indices", file=__file__, gh_indices = github_code_indices, ls=os.listdir(os.path.join(os.path.dirname(__file__),"indices")))
     return ghidra_index_locators + github_code_indices
 
